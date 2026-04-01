2b5e1c3b2768991b4d8410428958e1fad5eb2c77
Enzo Sgarbossa <137717016+esgarbossa@users.noreply.github.com>
2026-04-01 16:12:15 -0300

    Create Pir.java

---
+++ Pir.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Pir {
+    public static void main(String args[]){
+      Scanner in = new Scanner(System.in);
+      int n;
+      System.out.println("Altura");
+      n = in.nextInt();
+    }
+
